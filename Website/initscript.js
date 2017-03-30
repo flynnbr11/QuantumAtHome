d76e4e9e4322c868e35f09000043f9c02729e21a
Joe <joseph.lennon@bristol.ac.uk>
2017-03-30 19:14:25 +0100

    Added some common pages and basic template functionality

---
+++ Website/initscript.js
@@ -0,0 +1,47 @@
+// initscript.js
+// sets up the common elements for each page on the site
+
+function DoLoad() {
+	CreateNavbar();
+	LoadStyleSheets();
+	CreateFooter();
+}
+
+// create the footer
+function CreateFooter() {	
+}
+
+// loads all the commonly required stylesheets for each page
+function LoadStyleSheets () {
+	CreateStyleSheetLink("navbarStyles.css");
+}
+
+function CreateStyleSheetLink (filename) {
+	var linkItem = document.createElement("LINK");
+	
+	linkItem.setAttribute("rel", "stylesheet");
+	linkItem.setAttribute("type", "text/css");
+	linkItem.setAttribute("href", filename);
+	
+	document.head.insertBefore(linkItem, document.head.childNodes[0]);
+}
+
+// create a navbar item using the page name
+function CreateNavbarItem(name, navbar) {
+	var navbarItem = document.createElement("A");
+	
+	navbarItem.setAttribute("id", name);
+	navbarItem.setAttribute("class", "navbarItem");
+	navbarItem.setAttribute("href", name + ".html");
+	navbarItem.innerHTML = name;
+	navbar.insertBefore(navbarItem, navbar.childNodes[0]);
+}
+
+// create the navbar for each page
+function CreateNavbar() {
+	var navbar = document.getElementById("navbar");
+	
+	CreateNavbarItem("Contact", navbar);
+	CreateNavbarItem("About", navbar);
+	CreateNavbarItem("Index", navbar);
+}
