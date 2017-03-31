a5f22ad9f2a294d12f535d7900c6b554293b2630
Joe <joseph.lennon@bristol.ac.uk>
2017-03-31 20:39:47 +0100

    Added some comments to the animation test script

--- ExampleFunctionality/AnimationTest/animationScript.js
+++ ExampleFunctionality/AnimationTest/animationScript.js
@@ -1,3 +1,5 @@
+// example of how to implement animation using a canvas object, with love from Joe
+
 var animatedImage;
 
 function StartAnimationTest() {
@@ -8,6 +10,8 @@ function StartAnimationTest() {
 
 var AnimationTest = {
 	start : function() {
+		// this controls the rate at which the animated imaged cycles through frames
+		// the second parameter is the time in milliseconds
 		this.interval = setInterval(UpdateAnimation, 50);
 	}
 }
@@ -29,6 +33,13 @@ function AnimatedImage(frameWidth, frameHeight, frameCount, imgurl) {
 		var ctx = this.canvas.getContext("2d");
 		this.frame = (this.frame + 1) % this.frameCount;
 		this.x = this.frame * this.frameWidth;
+		
+		// draw a selected potion of the image on the canvas - the arguments are not in a logical order
+		// first argument is the image object itself
+		// second and third argument are the x,y start position in the IMAGE
+		// fourth and fifth are the width and height of the CANVAS to use
+		// sixth and seventh is the position relative to the CANVAS to place the image
+		// eigth and ninth are the size of the box in the IMAGE to draw
 		ctx.drawImage(this.img, this.x, 0, frameWidth, frameHeight, 0, 0, frameWidth, frameHeight);
 	}
 }
