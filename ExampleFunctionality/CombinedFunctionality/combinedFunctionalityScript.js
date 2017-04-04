8031fc51ee49c4ed408a0fb91661eace2e7db9b4
Joe <joseph.lennon@bristol.ac.uk>
2017-04-04 15:51:09 +0100

    Added some background images to combined func. example

--- ExampleFunctionality/CombinedFunctionality/combinedFunctionalityScript.js
+++ ExampleFunctionality/CombinedFunctionality/combinedFunctionalityScript.js
@@ -1,14 +1,17 @@
 // script combining functionality from other examples to demonstrate interplay
 
 function StartCombinedFunctionalityTest() {
-	MainArea.Load(100, 100, "");
+	MainArea.Load(800, 450, "");
 	
-	MainArea.AddChildRoom(new Room("living_room"));
+	MainArea.AddChildRoom("living_room", 50, 50, "images/bg_living_room.png");
+	MainArea.AddChildRoom("kitchen", 300, 50, "images/bg_kitchen.png");
 	
-	MainArea.GetRoomByName("living_room").AddChildFurniture(new Furniture(""));
+//	MainArea.GetRoomByName("living_room").AddChildFurniture(new Furniture(""));
 }
 
 // UI Engine
+
+// loading icon
 
 // main area
 // -- background
@@ -21,28 +24,27 @@ var MainArea = {
 		this.backgroundURL = bgurl;
 		this.childRooms = [];
 		this.clearColor = "lightgrey";
+		this.canvas.width = width;
+		this.canvas.height = height;
 		
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 		
 		this.context = this.canvas.getContext("2d");
 		
-		window.setInterval(UpdateAndDrawAll, 33);
+		this.interval = setInterval(UpdateAndDrawAll, 33);
 	},
-	AddChildRoom : function(room) {
-		this.childRooms.push(room);
+	AddChildRoom : function(name, x, y, bgimg) {
+		this.childRooms.push(new Room(name, x, y, bgimg));
 	},
 	UpdateAndDraw : function() {
+		var i, len;
+		
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 		this.context.fillStyle = this.clearColor;
 		this.context.fillRect(0,0,this.canvas.width, this.canvas.height);
 		
-		if (this.bObjectActive) {
-			this.activeObject.UpdateAndDraw();
-		}
-		else {
-			for (i = 0, len = this.childRooms.length; i < len; i++) {
-				this.childRooms[i].UpdateAndDraw(this.context);
-			}
+		for (i = 0, len = this.childRooms.length; i < len; i++) {
+			this.childRooms[i].UpdateAndDraw(this.context);
 		}
 	},
 	GetRoomByName : function(roomName) {
@@ -58,12 +60,10 @@ var MainArea = {
 // room object
 // -- background
 // -- furniture children
-function Room(name, x, y, width, height, bgurl) {
+function Room(name, x, y, bgurl) {
 	this.name = name;
 	this.x = x;
 	this.y = y;
-	this.width = width;
-	this.height = height;
 	this.bgurl = bgurl;
 	this.childFurniture = [];
 	
@@ -78,13 +78,14 @@ function Room(name, x, y, width, height, bgurl) {
 	}
 	
 	this.UpdateAndDraw = function (ctx) {
+		var i, len;
 	//	this.context.fillStyle = this.color;
 	//	this.context.fillRect(0,0,this.canvas.width, this.canvas.height);
 		
-	//	ctx.drawImage(this.bgimg, this.x, this.y, this.width, this.height);
+		ctx.drawImage(this.bgimg, this.x, this.y);
 		
 		for (i = 0, len = this.childFurniture.length; i < len; i++) {
-			this.childFurniture[i].UpdateAndDraw(i, this.x, this.y);
+			this.childFurniture[i].UpdateAndDraw(ctx, this.x, this.y);
 		}
 	}
 }
@@ -92,12 +93,13 @@ function Room(name, x, y, width, height, bgurl) {
 // furniture object
 // -- image (possibly animated)
 //	- controlled animation
-function Furniture(name, x, y) {
+function Furniture(name, x, y, imgurl) {
 	this.name	= name;
 	this.x		= x;
 	this.y		= y;
 	
 	this.UpdateAndDraw = function (ctx, xOffset, yOffset) {
+		
 	}
 }
 
