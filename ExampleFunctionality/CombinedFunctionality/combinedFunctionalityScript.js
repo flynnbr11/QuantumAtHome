a4dddb4fa9f8c179b6a779afe2a6684fcce4c828
Joe <joseph.lennon@bristol.ac.uk>
2017-04-04 14:30:31 +0100

    Added framework for the combined functionality example

---
+++ ExampleFunctionality/CombinedFunctionality/combinedFunctionalityScript.js
@@ -0,0 +1,17 @@
+// script combining functionality from other examples to demonstrate interplay
+
+function StartCombinedFunctionalityTest() {
+	
+}
+
+// main area
+// -- background
+// -- room children
+
+// room object
+// -- background
+// -- furniture children
+
+// furniture object
+// -- image (possibly animated)
+//	- controlled animation
