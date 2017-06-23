563f577a248a8ca0e5129c888b67c405869c60e2
Brian <flynnbr@tcd.ie>
2017-06-23 13:25:39 +0100

    Added functionality to click TV in Living room and go to XML segment.

--- Website/alpha/0.0.4/scripts/clickableCanvasTest.js
+++ Website/alpha/0.0.4/scripts/clickableCanvasTest.js
@@ -11,10 +11,11 @@ function Initialise(bgSrc) {
 	MainCanvas.Initialise(width, height, bgSrc);
 }
 
-function AddItemToCanvas(x, y, width, height, imgSrc, clickURL)
+function AddItemToCanvas(x, y, width, height, imgSrc, clickURL, isXML)
 {
-	MainCanvas.AddChild(new ClickableCanvasObject(x, y, width, height, imgSrc, clickURL));
+	MainCanvas.AddChild(new ClickableCanvasObject(x, y, width, height, imgSrc, clickURL, isXML));
 }
+
 
 var MainCanvas = {
 	// object holding information about the primary canvas
@@ -88,7 +89,7 @@ var MainCanvas = {
     }
 }
 
-function ClickableCanvasObject(x, y, width, height, imgSrc, url)
+function ClickableCanvasObject(x, y, width, height, imgSrc, url, isXML)
 {	
 	this.canvas = document.createElement("canvas");
 	this.baseX = x;
@@ -106,7 +107,7 @@ function ClickableCanvasObject(x, y, width, height, imgSrc, url)
 	this.img = new Image();
 	this.img.src = imgSrc;
 	this.url = url;
-    
+  this.isXML = isXML;
 	// sets the parent object of this object
 	this.SetParent = function(parentObj) {
 		this.parentObj	= parentObj;
@@ -115,9 +116,16 @@ function ClickableCanvasObject(x, y, width, height, imgSrc, url)
 	// function for responding to clicks handled by parent object
 	this.HandleClick = function(mx, my)
 	{
+			
+	
         if (mx > this.x && mx < this.canvas.width + this.x && my > this.y && my < this.y + this.canvas.height)
         {
-            window.location.href=this.url;
+        		if(isXML){
+        			loadXMLDoc(url);
+        		}
+        		else{
+	            window.location.href=this.url;
+						}
         }
 		/*
         if (this.bParentSet)
@@ -170,3 +178,107 @@ function ClickableCanvasObject(x, y, width, height, imgSrc, url)
 function UpdateArea() {
 	MainCanvas.Update();
 }
+
+
+
+var parser = new DOMParser();
+var xmlDoc;
+
+function loadXMLDoc(id) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      enableItemOverlay(id, this);
+    }
+  };
+  xmlhttp.open("GET", "living_room.xml" , true);
+  xmlhttp.send();
+}
+
+
+var text;
+
+/*function openOverlay(id, xml) {
+	var itemId;
+	
+	switch(id)
+	{
+	case 0:
+		itemId = "tv";
+		break;
+	case 1:
+		itemId = "phone";
+		break;
+	}
+	enableItemOverlay(itemId, xml);
+}*/
+
+var image;
+
+function enableItemOverlay(id, xml) {
+
+	var parent;
+	var name;
+	var tagline;
+	var summary;
+	var description;
+	
+	xmlDoc = parser.parseFromString(xml.responseText, "text/xml");
+	
+	var itemList = xmlDoc.getElementsByTagName("id");
+	var itemIndex;
+	
+	for (var i = 0; i < itemList.length; ++i)
+	{
+		if (itemList[i].innerHTML == id)
+		{
+			itemIndex = i;
+			break;
+		}
+	}
+	
+	image = new Image();
+	
+	parent = itemList[itemIndex].parentNode;
+	var i;
+	for (i = 0; i < parent.childNodes.length; ++i)
+	{
+		if (parent.childNodes[i].localName == "imgsrc")
+		{
+			image.src = parent.childNodes[i].innerHTML;
+		}
+		if (parent.childNodes[i].localName == "name")
+		{
+			name = parent.childNodes[i].innerHTML;
+		}
+		else if (parent.childNodes[i].localName == "tagline")
+		{
+			tagline = parent.childNodes[i].innerHTML;
+		}
+		else if (parent.childNodes[i].localName == "summary")
+		{
+			summary = parent.childNodes[i].innerHTML;
+		}
+		else if (parent.childNodes[i].localName == "description")
+		{
+			description = parent.childNodes[i].innerHTML;
+		}
+	}
+	
+	document.getElementById("itemImage").insertBefore(image, document.getElementById("itemImage").childNodes[0]);
+	document.getElementById("itemHeader").innerHTML = "<h1>" + name + "</h1>";
+	document.getElementById("itemTagline").innerHTML = "<p><i>" + tagline + "</i></p>";
+	document.getElementById("itemSummary").innerHTML = "<p>" + summary + "</p>";
+	document.getElementById("itemDescription").innerHTML = "<h2>Detailed Description</h2><p>" + description + "</p>";
+	
+	document.getElementById("itemOverlay").style.display = "flex";
+}
+
+function off() {
+	document.getElementById("itemOverlay").style.display = "none";
+	image.src = null;
+	document.getElementById("itemImage").innerHTML = "";
+}
+
+
+
