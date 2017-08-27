08a5006bef58aacd86e9d1fae9298fc3772eb4e9
Brian <flynnbr@tcd.ie>
2017-08-27 16:08:41 +0100

    Reordered Nav Bar

--- Website/alpha/0.0.6/scripts/repeatedCalls.js
+++ Website/alpha/0.0.6/scripts/repeatedCalls.js
@@ -1,6 +1,8 @@
 function LoadNavbar() {
-	document.getElementById("navbar").innerHTML+="<div class=\"logo_class\"> <a  href=\"index.html\"> <img width=\"10%\"  src=\"../images/qah_logo.png\"> </img> </a> </div>";
-	document.getElementById("navbar").innerHTML+="<a href=\"house.html\" class=\"navbarItem\">Home</a>";
+<!--	document.getElementById("navbar").innerHTML+="<div class=\"logo_class\"> <a  href=\"index.html\"> <img width=\"10%\"  src=\"../images/qah_logo.png\"> </img> </a> </div>"; -->
+
+	document.getElementById("navbar").innerHTML+="<a href=\"index.html\" class=\"navbarItem\">Homepage</a>";
+	document.getElementById("navbar").innerHTML+="<a href=\"house.html\" class=\"navbarItem\">Quantum@Home</a>";
 	document.getElementById("navbar").innerHTML+="<a href=\"quantum.html\" class=\"navbarItem\">Quantum Physics</a>";
 	document.getElementById("navbar").innerHTML+="<a href=\"all_items.html\" class=\"navbarItem\">Technologies</a>";
 	document.getElementById("navbar").innerHTML+="<a href=\"surveys.html\" class=\"navbarItem\">Surveys</a>";
