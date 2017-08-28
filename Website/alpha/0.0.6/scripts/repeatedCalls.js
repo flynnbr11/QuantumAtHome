163d993d5b1c7c7882d61b6bb34508af01a00594
Brian <flynnbr@tcd.ie>
2017-08-28 11:32:50 +0100

    added report link in navbar

--- Website/alpha/0.0.6/scripts/repeatedCalls.js
+++ Website/alpha/0.0.6/scripts/repeatedCalls.js
@@ -1,11 +1,12 @@
 function LoadNavbar() {
-<!--	document.getElementById("navbar").innerHTML+="<div class=\"logo_class\"> <a  href=\"index.html\"> <img width=\"10%\"  src=\"../images/qah_logo.png\"> </img> </a> </div>"; -->
+<!--	document.getElementById("navbar").innerHTML+=" <a  href=\"index.html\"> <img width=\"6%\"  src=\"../images/qah_logo.png\"> </img> </a>"; -->
 
-	document.getElementById("navbar").innerHTML+="<a href=\"index.html\" class=\"navbarItem\">Homepage</a>";
+  document.getElementById("navbar").innerHTML+="<a href=\"index.html\" class=\"navbarItem\">Homepage</a>"; 
 	document.getElementById("navbar").innerHTML+="<a href=\"house.html\" class=\"navbarItem\">Quantum@Home</a>";
 	document.getElementById("navbar").innerHTML+="<a href=\"quantum.html\" class=\"navbarItem\">Quantum Physics</a>";
 	document.getElementById("navbar").innerHTML+="<a href=\"all_items.html\" class=\"navbarItem\">Technologies</a>";
 	document.getElementById("navbar").innerHTML+="<a href=\"surveys.html\" class=\"navbarItem\">Surveys</a>";
+	document.getElementById("navbar").innerHTML+="<a href=\"QuantumAtHome.pdf\"  target=\"_blank\" class=\"navbarItem\">Report</a>";
 	document.getElementById("navbar").innerHTML+="<a href=\"about.html\" class=\"navbarItem\">About Us</a>";
 
 }
